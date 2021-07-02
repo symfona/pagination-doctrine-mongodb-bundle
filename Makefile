d3e90639960bbdc608816a84be718c85edfe7ea9
Sofy Korzhova <sofykorzhova@gmail.com>
2021-07-02 13:52:04 +0300

    Configured tests

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
-phpunit:
-	docker-compose run --rm -T php /usr/local/bin/php /app/vendor/bin/phpunit
+clear-cache:
+	rm -rf var
 
 phpstan:
 	docker-compose run --rm -T php /usr/local/bin/php /app/vendor/bin/phpstan analyse --no-progress
 
-coverage:
-	docker-compose run --rm -T php /usr/local/bin/php /app/vendor/bin/phpunit --coverage-html=vendor/coverage
+phpunit: clear-cache
+	docker-compose run --rm -T php /usr/local/bin/php /app/vendor/bin/phpunit
 
-composer-update:
+composer-update: clear-cache
 	docker-compose run --rm -T php /usr/local/bin/php /usr/local/bin/composer update
---
+++ tests/App/Kernel.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace Symfona\DemoBundle\Tests\App;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+
+final class Kernel extends \Symfony\Component\HttpKernel\Kernel
+{
+    use MicroKernelTrait;
+
+    public function registerBundles(): \Generator
+    {
+        yield new \Symfony\Bundle\FrameworkBundle\FrameworkBundle();
+        yield new \Symfona\DemoBundle\DemoBundle();
+    }
+
+    protected function configureContainer(ContainerConfigurator $container): void
+    {
+        $container->import('config/config.yaml');
+    }
+
+    protected function configureRoutes(RoutingConfigurator $routes): void
+    {
+        $routes->import('config/routes.yaml');
+    }
+}
